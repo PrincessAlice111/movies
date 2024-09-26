dd4752d1a283f790af6379c601d2a323e9d30ecf
Alice Delta Salgado Didier <es.salgadod@duocuc.cl>
2024-09-26 15:55:30 -0300

    Enable CORS

--- movies/src/main/java/com/example/movies/controller/MovieController.java
+++ movies/src/main/java/com/example/movies/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.example.movies.controller;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.movies.model.Movie;
 import com.example.movies.service.MovieService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
